aa6da66b9005ef9d9813e9479d7cd3e3ba1bc9dc
Alberto3001 <sealpardope@ittepic.edu.mx>
2025-07-28 10:25:50 -0700

    hola

--- automata.asm
+++ automata.asm
@@ -2,23 +2,15 @@
 .stack 100h
 
 .data
-    msg_accept db 'Cadena ACEPTADA', 0Dh, 0Ah, '$'
-    msg_reject db 'Cadena RECHAZADA', 0Dh, 0Ah, '$'
-    msg_accept_lcd db 'ACEPTADA', 0
-    msg_reject_lcd db 'RECHAZADA', 0
+    msg_accept db 'ACEPTADA', 0Dh, 0Ah, '$'
+    msg_reject db 'RECHAZADA', 0Dh, 0Ah, '$'
     input_prompt db 'Ingrese cadena: $'
     input_buffer db 255, 0, 255 dup(0)
     current_state db 0
     input_ptr dw 0
-    pda_stack db 256 dup(0)
-    sp_offset dw 1
-    stack_start db 'Z'
-    ; Transition format: estado_origen, input, pop, estado_destino, push_count, [símbolos]
-    pda_transitions db 0, 'a', 'Z', 0, 2, 'A', 'Z'
-                    db 0, 'a', 'A', 0, 2, 'A', 'A'
-                    db 0, 'b', 'A', 0, 0
-                    db 0, 'b', 'A', 0, 0
-                    db -1, -1, -1, -1, -1              ; Terminator
+    lcd_port dw 2040h  ; Puerto del LCD ASCII
+    accept_states_list db 2, -1
+    dfa_transitions db 0, 'a', 1, 1, 'b', 2, 2, 'a', 2, 2, 'b', 1, -1, -1, -1
 
 .code
 main proc
@@ -36,176 +28,119 @@ main proc
     mov dx, offset input_buffer
     int 21h
 
-    ; Initialize PDA
-    mov byte ptr [current_state], 0
-    mov word ptr [input_ptr], 0
-    mov al, [stack_start]
-    mov [pda_stack], al
-    mov word ptr [sp_offset], 1
+    mov al, 0
+    mov [current_state], al
+    mov [input_ptr], 0
 
 simulation_loop:
-    ; Check if we've processed all input
     mov cl, [input_buffer + 1]
     mov ch, 0
     mov ax, [input_ptr]
     cmp ax, cx
-    jge check_final_state
+    jge end_simulation
 
-    ; Get current input symbol
     mov bx, [input_ptr]
-    mov al, [input_buffer + 2 + bx]
-    ; Get stack top
-    mov di, [sp_offset]
-    cmp di, 0
-    je reject_final
-    mov dl, [pda_stack + di - 1]
-
-    ; Search for valid transition
-    mov si, offset pda_transitions
-
-find_transition:
+    mov bl, [input_buffer + 2 + bx]
+    mov si, offset dfa_transitions
+find_transition_loop:
     cmp byte ptr [si], -1
-    je reject_final
-    ; Check source state
-    mov ah, [current_state]
-    cmp [si], ah
-    jne next_transition
-    ; Check input symbol
-    cmp [si+1], al
-    jne next_transition
-    ; Check stack symbol
-    cmp [si+2], dl
-    jne next_transition
-    ; Transition found - execute
-    ; Update state
-    mov ah, [si+3]
-    mov [current_state], ah
-    ; Pop current symbol
-    mov di, [sp_offset]
-    cmp di, 0
-    je reject_final
-    dec word ptr [sp_offset]
-    ; Check if there are symbols to push
-    mov cl, [si+4]
-    cmp cl, 0
-    je advance_input
-    ; Push symbols in correct order (last to first)
-    mov ch, 0
-    mov bx, si
-    add bx, 5
-    add bx, cx
-    dec bx
-push_loop:
-    mov di, [sp_offset]
-    cmp di, 255
-    jge reject_final
-    mov dl, [bx]
-    mov [pda_stack + di], dl
-    inc word ptr [sp_offset]
-    dec bx
-    dec cl
-    jnz push_loop
-
-advance_input:
+    je reject_no_transition
+    mov al, [current_state]
+    cmp [si], al
+    jne next_dfa_entry
+    cmp [si+1], bl
+    jne next_dfa_entry
+    mov al, [si+2]
+    mov [current_state], al
     inc word ptr [input_ptr]
     jmp simulation_loop
+next_dfa_entry:
+    add si, 3
+    jmp find_transition_loop
 
-next_transition:
-    ; Calculate next transition position
-    mov cl, [si+4]
-    mov ch, 0
-    add si, 5
-    add si, cx
-    jmp find_transition
-
-check_final_state:
-    ; Check acceptance state (must be q0 = 0)
+reject_no_transition:
+    jmp reject_final
+end_simulation:
     mov al, [current_state]
-    cmp al, 0
-    jne reject_final
-
-    ; Check that stack contains only Z
-    mov di, [sp_offset]
-    cmp di, 1
-    jne reject_final
-
-    mov dl, [pda_stack]
-    cmp dl, 'Z'
-    jne reject_final
-
-    jmp accept_final
-; Procedimiento para inicializar el LCD
-init_lcd proc
-    mov dx, 2030h       ; Puerto de comandos del LCD
-    mov al, 38h         ; Modo 8 bits, 2 líneas, 5x7
-    out dx, al
-    mov al, 0Ch         ; Display ON, cursor OFF
-    out dx, al
-    mov al, 06h         ; Incremento automático, sin shift
-    out dx, al
-    mov al, 01h         ; Clear display
-    out dx, al
-    ret
-init_lcd endp
-
+    mov si, offset accept_states_list
+check_accept_loop:
+    mov bl, [si]
+    cmp bl, -1
+    je reject_final
+    cmp bl, al
+    je accept_final
+    inc si
+    jmp check_accept_loop
 accept_final:
-    ; Inicializar LCD
-    call init_lcd
-    ; Posicionar cursor al inicio
-    mov dx, 2030h       ; Puerto de comandos
-    mov al, 80h         ; Comando Set DDRAM Address
-    out dx, al
-    ; Mostrar mensaje en el LCD Display
-    mov dx, 2031h       ; Puerto de datos del LCD
-    mov si, offset msg_accept_lcd
-write_accept_lcd:
-    mov al, [si]        ; Cargar carácter
-    cmp al, 0           ; Verificar fin de cadena
-    je done_accept_lcd  ; Si es 0, terminamos
-    out dx, al          ; Enviar al LCD
-    ; Pequeño delay
-    push cx
-    mov cx, 1000
-delay1:
-    loop delay1
-    pop cx
-    inc si              ; Siguiente carácter
-    jmp write_accept_lcd
-done_accept_lcd:
-    ; Mostrar mensaje en consola
+    ; Mostrar salto de línea
+    mov ah, 02h
+    mov dl, 0Dh
+    int 21h
+    mov dl, 0Ah
+    int 21h
+
+    ; Mostrar palabra 'Cadena' y espacio
+    mov ah, 02h
+    mov dl, 'C'
+    int 21h
+    mov dl, 'a'
+    int 21h
+    mov dl, 'd'
+    int 21h
+    mov dl, 'e'
+    int 21h
+    mov dl, 'n'
+    int 21h
+    mov dl, 'a'
+    int 21h
+    mov dl, ' '
+    int 21h
+
+    ; Mostrar mensaje de aceptación
     mov ah, 09h
     mov dx, offset msg_accept
     int 21h
+
+    ; Mostrar 'A' en LCD ASCII
+    mov dx, [lcd_port]
+    mov al, 'A'
+    out dx, al
     jmp exit_program
 
 reject_final:
-    ; Inicializar LCD
-    call init_lcd
-    ; Posicionar cursor al inicio
-    mov dx, 2030h       ; Puerto de comandos
-    mov al, 80h         ; Comando Set DDRAM Address
-    out dx, al
-    ; Mostrar mensaje en el LCD Display
-    mov dx, 2031h       ; Puerto de datos del LCD
-    mov si, offset msg_reject_lcd
-write_reject_lcd:
-    mov al, [si]        ; Cargar carácter
-    cmp al, 0           ; Verificar fin de cadena
-    je done_reject_lcd  ; Si es 0, terminamos
-    out dx, al          ; Enviar al LCD
-    ; Pequeño delay
-    push cx
-    mov cx, 1000
-delay2:
-    loop delay2
-    pop cx
-    inc si              ; Siguiente carácter
-    jmp write_reject_lcd
-done_reject_lcd:
-    ; Mostrar mensaje en consola
+    ; Mostrar salto de línea
+    mov ah, 02h
+    mov dl, 0Dh
+    int 21h
+    mov dl, 0Ah
+    int 21h
+
+    ; Mostrar palabra 'Cadena' y espacio
+    mov ah, 02h
+    mov dl, 'C'
+    int 21h
+    mov dl, 'a'
+    int 21h
+    mov dl, 'd'
+    int 21h
+    mov dl, 'e'
+    int 21h
+    mov dl, 'n'
+    int 21h
+    mov dl, 'a'
+    int 21h
+    mov dl, ' '
+    int 21h
+
+    ; Mostrar mensaje de rechazo
     mov ah, 09h
     mov dx, offset msg_reject
     int 21h
+
+    ; Mostrar 'R' en LCD ASCII
+    mov dx, [lcd_port]
+    mov al, 'R'
+    out dx, al
 
 exit_program:
     mov ah, 4ch
